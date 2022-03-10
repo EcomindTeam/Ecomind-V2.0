d97325706e6742489e832f9331d0d86206ec3da0
gsguimaraes <gs.guimaraes@unesp.br>
2022-03-10 20:30:56 -0300

    Conexão alterada para localhost

--- Ecomind/conexao/conexao.php
+++ Ecomind/conexao/conexao.php
@@ -1,5 +1,5 @@
 <?php
-    $conecta = new PDO("pgsql:host=localhost port=5432 dbname=b30samaral", "user=b30samaral", "password=cti", array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+    $conecta = new PDO("pgsql:host=localhost port=5432 dbname=Ecomind", "postgres", "alunocti", array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
     if (!$conecta)
     {
         echo "Erro fatal: Não foi possível estabelecer conexão com o banco de dados!<br><br>";	
