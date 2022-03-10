cd11d9d496854313c678b61a89fa93f29cf37313
henrique_alonso <101106251+henriquedalonso@users.noreply.github.com>
2022-03-10 08:33:51 -0400

    ferrou

--- Ecomind/insercao/insere_usuario.php
+++ Ecomind/insercao/insere_usuario.php
@@ -52,7 +52,8 @@
                 $nome = $_POST['nome'];
                 $cpf = $_POST['cpf'];
 		        $endereco = $_POST['endereco'];
-                $sql="INSERT INTO usuario VALUES (DEFAULT,'$senha','$nome','$email','$cpf','$endereco')";
+                // Mudar aqui
+                //$sql="INSERT INTO usuario VALUES (DEFAULT,'$senha','$nome','$email','$cpf','$endereco')";
                 $resultado= pg_query($conecta, $sql);
                 $qtde=pg_affected_rows($resultado);
                 $erro=pg_last_error();
